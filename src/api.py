98aade66358b536eb800c4bbc47916623effe506
BIMseven <bim7@pitt.edu>
2019-03-11 15:06:40 -0400

    ran first unit test and go 42

--- src/api.py
+++ src/api.py
@@ -10,8 +10,28 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
 )
 # Suppress deprecation warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 db.init_app(app)
+# -----------------------------------------------------------------------------CLI-Commands
+@app.cli.command("initdb")
+def init_db():
+    """Initializes database and any model objects necessary for assignment"""
+    db.drop_all()
+    db.create_all()
+
+    print("Initialized Freelance-Tracker Database.")
+
+
+@app.cli.command("devinit")
+def init_dev_data():
+    """Initializes database with data for development and testing"""
+    db.drop_all()
+    db.create_all()
+    print("Initialized Freelance-Tracker Database.")
+
+    db.session.commit()
+    print("Added dummy data.")
+
+# -----------------------------------------------------------------------------------/CLI Commands
 
 
 @app.route('/')
@@ -22,48 +42,49 @@ def backend():
 # Pulls json from a request
 def pull_json():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 # Send the jobs of the passed in author
 def get_jobs():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 # Output billing period to csv
 def output_to_csv():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 # Creates a new job
 # args: cuser, author, type, task, company
 def create_job():
     # TODO: IMPLEMENT
+    raise NotImplementedError
     return
 
 
 def update_job():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 def check_auth():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 def requres_auth():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 def authenticate():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
 
 
 def create_user():
     # TODO: IMPLEMENT
-    return
+    raise NotImplementedError
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,9 +1,26 @@
 from flask import Flask, request
-from src import api
+from src import api, models
 import pytest
 
 
-def test_json_from_req():
-    req = request()
-    req.method = 'POST'
-    pass
+@pytest.fixture()
+def make_user():
+    """Adds a user to the database to ensure we have something to add"""
+    return
+
+
+@pytest.fixture()
+def initdb():
+    """Initializes the database to ensure we have a clean working environment"""
+    api.init_db
+    return 42
+
+
+def test_initdb(initdb):
+    assert initdb == 42
+
+
+def test_create_job():
+    """ tests that creat_job returns a job json object"""
+    # api.create_job(user, client, start_date, due_date, rate)
+    return
